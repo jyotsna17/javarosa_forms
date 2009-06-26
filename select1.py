ceecd5f445d51d14162a7c094ca82b41415bf568
Jyotsna Sebe <jyotsna.reddy17@gmail.com>
2009-06-26 15:07:27 -0400

    git commit select1.py

--- select1.py
+++ select1.py
@@ -1,24 +1,24 @@
 import xml.dom.minidom
 
 class select1:
+
     
     def __init__(self , doc):
         self.doc = doc
-        self.count_of_nodes = 0;5
+        self.items = 0
 
     def add_select1_node(self, question):    
-        self.select1 = self.doc.createElement("select")
+        self.select1 = self.doc.createElement("select1")
         self.select1_label = self.doc.createElement("label")
         self.select1_label.appendChild(self.doc.createTextNode(question))
         self.select1.appendChild(self.select1_label)
         self.doc.appendChild(self.select1)
         
  
-    def add_item(self, label_val,value_val):
+    def add_item(self, label_val , value_val):
 
-        if(self.count_of_nodes < 2):
-            # add an item to the select node
-
+        # add an item to the select node
+        if(self.items < 2):
             item = self.doc.createElement("item")
             label = self.doc.createElement("label")
             value = self.doc.createElement("value")
@@ -27,13 +27,9 @@ class select1:
             item.appendChild(label)
             item.appendChild(value)
             self.select1.appendChild(item)
+            self.items = self.items +1
             print self.doc.toprettyxml()
-            self.count_of_nodes = self.count_of_nodes +1  
-
-            #need to throw the corresponding exception if more than 2 items are added
-
-        
-        
+            
         
         
 
