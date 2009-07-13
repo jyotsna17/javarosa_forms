8916f51e5c456b22a3b7181207454ce75f0c178b
Jyotsna Sebe <jyotsna.reddy17@gmail.com>
2009-07-13 16:35:08 -0400

    modified:   nodeset.py 	modified:   test_script.py

--- test_script.py
+++ test_script.py
@@ -7,6 +7,17 @@ import xml.dom.minidom
 
 
 obj = Xtree("Test Form")
+model_node = obj.return_model_node()
+instance_node = instance(obj.doc,"household")
+instance_node.add_instance_nodes("name")
+instance_node.add_instance_nodes("age")
+model_node.appendChild(instance_node.return_instance_node())
+nodeset1 = nodeset(obj.doc,"name")
+nodeset1.update_required("true")
+model_node.appendChild(nodeset1.return_object())
+nodeset2 = nodeset(obj.doc,"age")
+nodeset2.update_required("true")
+model_node.appendChild(nodeset2.return_object())
 body = obj.return_body_node()
 grp_element = group(obj.doc,"person's details")
 input_item = inputs(obj.doc,"Enter5 patients name")
@@ -18,4 +29,8 @@ select1_item.add_item("no","no")
 
 grp_element.add_group_item(select1_item.return_object())
 body.appendChild(grp_element.return_object())
-print obj.doc.toprettyxml()
+
+f = open('test.xml', 'w')
+obj.doc.writexml(f)
+f.close()
+
